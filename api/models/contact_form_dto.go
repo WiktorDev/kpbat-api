6c06fa4c58bfe96c72b29f5d09a0af6cd830fb8f
Wiktor <mcserw4@wp.pl>
2023-10-29 11:16:56 +0100

    contact form

--- api/models/contact_form_dto.go
+++ api/models/contact_form_dto.go
@@ -1,23 +1,36 @@
 package models
 
-import "kpbatApi/api/base/utils"
+import (
+	"kpbatApi/api/base/utils"
+	"regexp"
+)
 
 type ContactForm struct {
 	Email       string `json:"email"`
-	PhoneNumber string `json:"phoneNumber"`
+	PhoneNumber string `json:"phone_number"`
+	FirstName   string `json:"first_name"`
+	LastName    string `json:"last_name"`
 	Subject     string `json:"subject"`
 	Message     string `json:"message"`
 }
 
 func ContactFormValidator(bind *ContactForm) utils.Validated {
-	if len(bind.Email) < 6 {
-		return utils.Validated{Message: "email length must be longer than 6 characters"}
+	if !isValidPattern("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$", bind.Email) {
+		return utils.Validated{Message: "Invalid email format!"}
 	}
-	if len(bind.Subject) < 12 {
-		return utils.Validated{Message: "subject length must be longer than 12 characters"}
+	if len(bind.FirstName) < 3 {
+		return utils.Validated{Message: "Invalid first name!"}
+	}
+	if len(bind.LastName) < 3 {
+		return utils.Validated{Message: "Invalid last name!"}
 	}
 	if len(bind.Message) < 20 {
 		return utils.Validated{Message: "message length must be longer than 12 characters"}
 	}
 	return utils.Validated{Ok: true}
+}
+
+func isValidPattern(pattern string, text string) bool {
+	match, _ := regexp.MatchString(pattern, text)
+	return match
 }
--- api/routing/contact.go
+++ api/routing/contact.go
@@ -19,16 +19,22 @@ import (
 // @Router		/contact [post]
 func sendMail(ctx echo.Context) error {
 	var config = kpbatApi.GetConfig()
-	bind := new(models.ContactForm)
-	if err := ctx.Bind(bind); err != nil {
-		return utils.HttpError(ctx, http.StatusBadRequest, utils.Message(err.Error()))
+	bind := models.ContactForm{
+		FirstName:   ctx.FormValue("first_name"),
+		LastName:    ctx.FormValue("last_name"),
+		Email:       ctx.FormValue("email"),
+		PhoneNumber: ctx.FormValue("phone_number"),
+		Subject:     ctx.FormValue("subject"),
+		Message:     ctx.FormValue("message"),
 	}
-
-	err, isValid := utils.Validate(ctx, models.ContactFormValidator(bind))
+	err, isValid := utils.Validate(ctx, models.ContactFormValidator(&bind))
 	if err != nil {
 		return err
 	}
 	if isValid {
+		if err := kpbatApi.SendMail(bind.Email, "Message sent!", "template_to_sender.html", nil); err != nil {
+			return utils.HttpError(ctx, http.StatusInternalServerError, utils.Message(err.Error()))
+		}
 		if err := kpbatApi.SendMail(config.Mail.AdminMail, "New message from "+bind.Email, "template_to_system.html", bind); err != nil {
 			return utils.HttpError(ctx, http.StatusInternalServerError, utils.Message(err.Error()))
 		}
